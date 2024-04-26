e1c3c864c8abdd828390504ba318821988746695
Swarnavo Mukherjee <swarnavomukherjee03@gmail.com>
2024-04-26 16:46:44 +0530

    z

--- agent.py
+++ agent.py
@@ -78,8 +78,8 @@ class Agent:
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
+        for state, action, reward, nexrt_state, done in mini_sample:
+            self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
